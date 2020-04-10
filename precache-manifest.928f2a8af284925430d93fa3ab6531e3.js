c0c4c2af2f3d10dcd71df2ea560d7b968f2fff44
cod4adm <cod4adm@gmail.com>
2020-04-10 20:59:57 +0300

    gh-pages

---
+++ precache-manifest.928f2a8af284925430d93fa3ab6531e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36af85855e095312e917c501b6bc4525",
+    "url": "/kinoafisha/index.html"
+  },
+  {
+    "revision": "848fbe243ec11679640a",
+    "url": "/kinoafisha/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e366fa4ed33e0873e23f",
+    "url": "/kinoafisha/static/css/main.734b4a8a.chunk.css"
+  },
+  {
+    "revision": "848fbe243ec11679640a",
+    "url": "/kinoafisha/static/js/2.d71a9854.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kinoafisha/static/js/2.d71a9854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e366fa4ed33e0873e23f",
+    "url": "/kinoafisha/static/js/main.29ae1067.chunk.js"
+  },
+  {
+    "revision": "d606bf0555d1a08fd292",
+    "url": "/kinoafisha/static/js/runtime-main.d82a60a0.js"
+  }
+]);
